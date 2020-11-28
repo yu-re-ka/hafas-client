c14080e8b98f8d013bbf33ecd1d753063551217a
Jannis R <mail@jannisr.de>
2020-11-28 15:43:05 +0100

    basic subscription handling

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const isObj = require('lodash/isObject')
 const sortBy = require('lodash/sortBy')
 const pRetry = require('p-retry')
 const omit = require('lodash/omit')
+const {v4: uuidV4} = require('uuid')
+const {randomBytes} = require('crypto')
+const {gunzipSync} = require('zlib')
 
 const defaultProfile = require('./lib/default-profile')
 const validateProfile = require('./lib/validate-profile')
@@ -554,6 +557,108 @@ const createClient = (profile, userAgent, opt = {}) => {
 		}
 	}
 
+	const _checkSubscriptionsResultCode = (res) => {
+		if (!res.result || res.result.resultCode !== 'OK') {
+			const err = new Error('unexpected reponse')
+			err.res = res
+			throw err
+		}
+	}
+
+	// This HAFAS call seems to be idempotent.
+	// user IDs are case-sensitive!
+	const createSubscriptionsUser = async (channelIds = [uuidV4()], opt = {}) => {
+		if (!Array.isArray(channelIds) || channelIds.length === 0) {
+			throw new TypeError('channelIds must be a non-empty array')
+		}
+		for (let i = 0; i < channelIds.length; i++) {
+			if (!isNonEmptyString(channelIds[i])) {
+				throw new TypeError(`channelIds[${i}] must be a non-empty string`)
+			}
+		}
+
+		const pushToken = opt.pushToken || randomBytes(32).toString('hex')
+		const channels = channelIds.map((channelId) => ({
+			type: 'IPHONE',
+			name: 'PUSH_IPHONE',
+			address: pushToken,
+			channelId,
+			options: [
+				{type: 'NO_SOUND', value: '1'},
+				// todo: move to profiles?
+				// {type: 'CUSTOMER_TYPE', value: 'com.deutschebahn.vrn'},
+			],
+		}))
+		const {res} = await profile.request({profile, opt}, userAgent, {
+			meth: 'SubscrUserCreate',
+			req: {
+				userId: opt.userId || uuidV4(),
+				language: 'en', // todo: make customizable
+				channels,
+			},
+		})
+		_checkSubscriptionsResultCode(res)
+		return res.userId
+	}
+
+	const subscriptions = async (userId) => {
+		if (!isNonEmptyString(userId)) {
+			throw new TypeError('userId must be a non-empty string')
+		}
+
+		const {res} = await profile.request({profile, opt}, userAgent, {
+			meth: 'SubscrSearch',
+			req: {
+				userId,
+			},
+		})
+		_checkSubscriptionsResultCode(res)
+
+		const parseChannel = (ch) => ({
+			id: ch.channelId,
+		})
+		const parseSubscription = (sub) => ({
+			id: sub.subscrId,
+			status: sub.status,
+			channels: sub.channels.map(parseChannel),
+			journeyRefreshToken: sub.ctxRecon,
+		})
+
+		if (!Array.isArray(res.conSubscrL)) return []
+		return res.conSubscrL.map(parseSubscription)
+	}
+
+	const subscription = async (userId, subscriptionId) => {
+		if (!isNonEmptyString(userId)) {
+			throw new TypeError('userId must be a non-empty string')
+		}
+		if (subscriptionId === null || subscriptionId === undefined) {
+			throw new TypeError('missing subscriptionId')
+		}
+
+		const {res} = await profile.request({profile, opt}, userAgent, {
+			meth: 'SubscrDetails',
+			req: {
+				userId,
+				subscrId: subscriptionId,
+			},
+		})
+		_checkSubscriptionsResultCode(res)
+
+		return {
+			id: subscriptionId,
+			journeyRefreshToken: res.conSubscr.ctxRecon,
+			// todo: serviceDays
+			hysteresis: res.conSubscr.hysteresis,
+			monitorFlags: res.conSubscr.monitorFlags,
+			// todo: data
+			connectionInfo: res.conSubscr.connectionInfo,
+			// todo: parse & give a proper name
+			rtEvents: res.eventHistory && res.eventHistory.rtEvents || null,
+			himEvents: res.eventHistory && res.eventHistory.himEvents || null,
+		}
+	}
+
 	const client = {
 		departures,
 		arrivals,
@@ -570,6 +675,11 @@ const createClient = (profile, userAgent, opt = {}) => {
 	if (profile.tripsByName) client.tripsByName = tripsByName
 	if (profile.remarks !== false) client.remarks = remarks
 	if (profile.lines !== false) client.lines = lines
+	if (profile.subscriptions !== false) {
+		client.createSubscriptionsUser = createSubscriptionsUser
+		client.subscriptions = subscriptions
+		client.subscription = subscription
+	}
 	Object.defineProperty(client, 'profile', {value: profile})
 	return client
 }
